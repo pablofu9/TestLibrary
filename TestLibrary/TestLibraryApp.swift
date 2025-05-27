d8cbce402adb6dca6a6c9440c38c139e5a953adc
Pablofu9 <pfuertes.ruiz.96@gmail.com>
2025-05-27 09:40:07 +0200

    Initial Commit

---
+++ TestLibrary/TestLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLibraryApp.swift
+//  TestLibrary
+//
+//  Created by Pablo Fuertes ruiz on 27/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TestLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
